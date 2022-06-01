a5f97eabe21c3db0cbd2e1c6ce5f8d72673add8a
kartikk221 <kartikk221@yahoo.com>
2022-06-01 17:15:00 -0400

    Initialized Components, Finished Raw Parser Component, Finished Request Validator & Shared Body Buffer Parser

--- src/components/raw.js
+++ src/components/raw.js
@@ -1,14 +1,17 @@
 const bytes = require('bytes');
 const type_is = require('type-is');
 
+const { validate_request, get_body_buffer } = require('../shared.js');
+
 /**
+ * Creates a raw (Buffer) body parsing middleware which will parse incoming body data into the Request.body property under provided option based conditions.
  *
  * @param {Object} options - Options object for Raw body parser
  * @param {Boolean} options.inflate When set to `true`, then deflated (compressed) bodies will be inflated; when `false`, deflated bodies are rejected. Defaults to `true`.
  * @param {String|Number} options.limit Controls the maximum request body size. If this is a number, then the value specifies the number of bytes; if it is a string, the value is passed to the bytes library for parsing. Defaults to '100kb'.
  * @param {String|Function} options.type The `type` option is used to determine what media type the middleware will parse. Can be a string or a function. If a function, it will be passed the `Request` object and should return a boolean. If a string, it will be used to match the media type. Defaults to 'application/octet-stream'.
  */
-function get_raw_parser(options = {}) {
+function create_raw_parser(options = {}) {
     // Destructure the options object with defaults
     const { inflate = true, limit = '100kb', type = 'application/octet-stream', verify } = options;
 
@@ -21,16 +24,28 @@ function get_raw_parser(options = {}) {
         throw new Error('HyperExpress.BodyParser.raw(options) -> options.verify must be a function');
 
     // Generate parsed properties from the options utilized during parsing
-    const body_inflate = inflate === true;
-    const body_type_check = typeof type === 'function' ? type : (req) => type_is(req, type);
-    const body_limit_bytes = typeof limit === 'number' ? limit : bytes.parse(limit);
+    const conditions = {
+        base_encoding: null,
+        verify_body: verify,
+        inflate: inflate === true,
+        limit: typeof limit === 'number' ? limit : bytes.parse(limit),
+        match_type: typeof type === 'function' ? type : (req) => Boolean(type_is(req, type)),
+    };
 
     // Return the middleware function
     return async (request, response) => {
         // Initialize the body property to an empty Buffer if it doesn't exist
         if (!request.body) request.body = Buffer.allocUnsafe(0);
 
-        // Ensure that the request has a body
-        if (!type_is.hasBody(request)) return;
+        // Validate this request to determine if we should parse it
+        // Note: This method will automatically send the appropriate error HTTP responses under appropriate scenarios
+        if (validate_request(request, conditions)) {
+            // Attempt to read the incoming request body as a Buffer
+            const buffer = await attempt_body_buffer(request, response, conditions);
+            if (buffer) {
+                // Write the body buffer to the request body property
+                request.body = buffer;
+            }
+        }
     };
 }
---
+++ src/shared.js
@@ -0,0 +1,155 @@
+const zlib = require('zlib');
+const iconv = require('iconv-lite');
+const unpipe = require('unpipe');
+const destroy = require('destroy');
+const type_is = require('type-is');
+const raw_body = require('raw-body');
+
+/**
+ * @typedef {Object} ParserConditions
+ * @property {Boolean} inflate - Whether or not to inflate the body.
+ * @property {Number} limit - The maximum size of the body in bytes.
+ * @property {function():boolean} match_type - A function that returns whether the incoming request should be parsed or not based on content type.
+ * @property {function():boolean=} verify_body - A function that returns whether the incoming body should be parsed or not based on the request.
+ * @property {String=} base_encoding - The base encoding to use when parsing the incoming body.
+ */
+
+/**
+ * Returns whether an incoming request should be parsed or not.
+ *
+ * @param {HyperExpress.Request} request
+ * @param {ParserConditions} conditions
+ * @returns {Boolean}
+ */
+function validate_request(request, conditions) {
+    // Destructure the conditions object with relevant properties
+    const { match_type } = conditions;
+
+    // Do not parse the request if it has already been parsed
+    if (request._body === true) return false;
+
+    // Do not parse the request if there is no body to parse
+    if (!type_is.hasBody(request)) return false;
+
+    // Do not parse the request if it doesn't match the appropriate type
+    if (!match_type(request)) return false;
+
+    // Mark this request as "parsed" with a boolean flag
+    request._body = true;
+    return true;
+}
+
+/**
+ * Attempts to retrieve the body buffer from the incoming request.
+ * Automatically asserts and applies the provided set of conditions.
+ *
+ * @param {HyperExpress.Request} request
+ * @param {HyperExpress.Response} response
+ * @param {ParserConditions} conditions
+ * @returns {Promise<Buffer|undefined>}
+ */
+async function attempt_body_buffer(request, response, conditions) {
+    // Destructure appropriate properties from the conditions object
+    const { inflate, limit, verify_body, base_encoding } = conditions;
+
+    // Determine content properties from the request
+    const length = request.headers['content-length'];
+    const encoding = (request.headers['content-encoding'] || 'identity').toLowerCase();
+
+    // Return an HTTP 415 error as decompression of the incoming encoding is not supported
+    if (inflate === false && encoding !== 'identity') return response.status(415).send();
+
+    // Attempt to stream the incoming body from HyperExpress with the provided conditional limit
+    if (request._stream_with_limit(limit)) {
+        // Read the incoming body data into a buffer
+        let buffer;
+        if (encoding === 'identity') {
+            // Retrieve the raw body buffer directly from HyperExpress
+            // This is more memory efficient than consuming/processing the chunks from the request stream
+            buffer = await request.buffer();
+        } else {
+            // Initialize a decompression stream which will decompress the incoming body data
+            let stream;
+            switch (encoding) {
+                case 'deflate':
+                    // Create a deflate decompression stream and pipe the request stream into it
+                    stream = zlib.createInflate();
+                    request.pipe(stream);
+                    break;
+                case 'gzip':
+                    // Create a gzip decompression stream and pipe the request stream into it
+                    stream = zlib.createGunzip();
+                    request.pipe(stream);
+                    break;
+                default:
+                    // Return an HTTP 415 error as the encoding is not supported
+                    return response.status(415).send();
+            }
+
+            // Read the decompression stream into a buffer
+            try {
+                // Utilize the raw-body module to read the decompression stream into a buffer
+                buffer = await new Promise((resolve, reject) =>
+                    raw_body(
+                        stream,
+                        {
+                            length, // The length of the incoming body data
+                            limit, // The maximum size of the incoming body data in bytes
+                            encoding: typeof verify_body == 'function' ? null : base_encoding,
+                        },
+                        (error, body) => {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                resolve(body);
+                            }
+                        }
+                    )
+                );
+            } catch (error) {
+                // Destroy the decompression stream
+                destroy(stream, true);
+
+                // Unpipe destinations from the request readable stream
+                unpipe(request);
+
+                // Flush any remaining data from the decompression stream
+                request._stream_flush();
+
+                // Handle the error type and respond to the request with appropriate response
+                switch (error.type) {
+                    case 'encoding.unsupported':
+                        // Return an HTTP 415 error as the encoding is not supported
+                        return response.status(415).send();
+                    default:
+                        // Return an HTTP 400 error as this is a bad request according to raw-body module
+                        return response.status(400).send();
+                }
+            }
+
+            // Verify the received buffer against the provided verify_body function
+            if (typeof verify_body == 'function' && verify_body(request, response, buffer, encoding) !== true) {
+                // Return an HTTP 403 error as the body verification failed
+                return response.status(403).send();
+            }
+
+            // Decode the buffer from the provided base encoding
+            if (base_encoding) {
+                try {
+                    // Attempt to decode the buffer from the provided base encoding with the iconv-lite module
+                    buffer = iconv.decode(buffer, base_encoding);
+                } catch (error) {
+                    // Return an HTTP 400 error as the encoding parsing failed
+                    return response.status(400).send();
+                }
+            }
+        }
+
+        return buffer;
+    }
+}
+
+module.exports = {
+    validate_request,
+    get_body_buffer,
+};
